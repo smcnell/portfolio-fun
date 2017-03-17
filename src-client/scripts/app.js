0b1d740b5c05250bc74a22a128669e07ae02e143
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-03-16 21:30:23 -0400

    initial commit

--- src-client/scripts/app.js
+++ src-client/scripts/app.js
@@ -1,5 +1,14 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-	
-ReactDOM.render(<h1>boo yah!</h1>, document.querySelector('#app-container'))
 
+const AppRouter = Backbone.Router.extend({
+  routes: {
+    "" : "showHomePage",
+    "about" : "showAboutMePage",
+    "projects" : "showProjectsPage",
+		"skills": "showSkillsPage"
+  },
+
+ReactDOM.render(<h1> We are da shit</h1>, document.querySelector('#app-container'))
+
+new AppRouter()
